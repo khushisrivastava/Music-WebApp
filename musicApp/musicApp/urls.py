28b3dd22b60c4d0d97f73f4f3dd24bfc403fd187
Khushi <khushisrivastava53@gmail.com>
2020-07-30 01:12:26 +0530

    Redesigned navbar Configured static folder Moved template and static files in the root

--- musicApp/musicApp/urls.py
+++ musicApp/musicApp/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('web/', include('musicweb.urls')),
-    path('api/', include('musicapi.urls'))
+    path('api/', include('musicapi.urls')),
 ]
--- musicApp/musicweb/urls.py
+++ musicApp/musicweb/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', TemplateView.as_view(template_name='../template/base.html'), name='home'),
 ]
